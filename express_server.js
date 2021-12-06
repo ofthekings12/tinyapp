093467b1f3069ac690f953b91cb9c33c21215c44
Mikko De Los Reyes <mikkode212@gmail.com>
2021-12-06 19:27:44 +0000

    added additional endpoint to /urls.json

--- express_server.js
+++ express_server.js
@@ -6,14 +6,14 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
