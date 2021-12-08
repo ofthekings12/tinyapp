bf3b7eac26834ed6114f74b6e0097320ff638cea
Mikko De Los Reyes <mikkode212@gmail.com>
2021-12-08 15:02:41 +0000

    lost a bunch of changes & had a mental breakdown. but i fixed it now i'm only partially upset

--- express_server.js
+++ express_server.js
@@ -1,81 +1,100 @@
 function generateRandomString() {
-  var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < characters.length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result.slice(0, 6);
+  return Math.random().toString(36).slice(2, 8);
 }
-
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls/new", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
+  res.render("urls_show.ejs", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.statusCode = 404;
+    res.send('404 Invalid short URL');
+  }
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-})
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-})
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
-});
-
-app.post("/urls", (req, res) => {
- const shortUrl = generateRandomString() 
- const longUrl = req.body.longURL
- urlDatabase[shortUrl] = longUrl
-  console.log(urlDatabase);
-  res.redirect(`/urls/${shortUrl}`);
-});
-
-app.post("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL
-
-  res.redirect(`/urls/${shortURL}`)
+app.get("/", (req, res) => {
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-const shortURL = req.params.shortURL
+  const shortURLToDelete = req.params.shortURL;
+  const longURLToDelete = urlDatabase[shortURLToDelete];
+  if (longURLToDelete) {
+    delete urlDatabase[shortURLToDelete];
+    res.redirect('/urls');
+  } else {
+    res.statusCode = 500;
+    res.send('500 Server error');
+  }
+});
 
-delete urlDatabase[shortURL];
-  res.redirect("/urls");
-})
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+app.post("/urls", (req, res) => {
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
+});
 
-})
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  if (username.length) {
+    res.cookie('username', username)
+      .redirect('/urls/');
+  } else {
+    res.redirect('/urls/');
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+    .redirect('/urls/');
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
